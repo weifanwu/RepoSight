6b0d1d14454007ed4974205ec1c7c7a51afa2620
weifan <weifan@uw.edu>
2024-11-23 20:11:14 +0800

    setup config allows larger body parser

--- components/uploader/fileUploader.tsx
+++ components/uploader/fileUploader.tsx
@@ -61,9 +61,6 @@ export default function FolderUpload({ onUploadComplete }: FolderUploadProps) {
       const response = await fetch("/api/diagram", {
         method: "POST",
         body: formData,
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
       });
 
       if (response.ok) {
